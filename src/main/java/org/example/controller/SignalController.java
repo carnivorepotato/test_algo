3913418c175466894f1b04073378fe081e9c4192
Vadim Iakovlev <carnivorepotato@gmail.com>
2023-08-04 20:06:54 +0200

    endpoint is actually POST now

--- src/main/java/org/example/controller/SignalController.java
+++ src/main/java/org/example/controller/SignalController.java
@@ -4,10 +4,7 @@ import org.example.service.SignalHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("signal")
@@ -15,7 +12,7 @@ public class SignalController {
     @Autowired
     private SignalHandler signalHandler;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity handleSignalById(@RequestParam Integer signal) {
         signalHandler.handleSignal(signal);
         return new ResponseEntity<>(HttpStatus.OK);
